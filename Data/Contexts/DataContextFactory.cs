70f95908a3f4a018b1ba292614eab8e059b416f8
Nour <deen80@live.com>
2024-05-21 11:46:24 +0200

    fix dataBase

--- Data/Contexts/DataContextFactory.cs
+++ Data/Contexts/DataContextFactory.cs
@@ -9,7 +9,7 @@ public class DataContextFactory : IDesignTimeDbContextFactory<DataContext>
     public DataContext CreateDbContext(string[] args)
     {
         var optionsBuilder = new DbContextOptionsBuilder<DataContext>();
-        optionsBuilder.UseSqlServer("Server=tcp:sql-server-silicon.database.windows.net,1433;Initial Catalog=silicon_database;Persist Security Info=False;User ID=sqlAdmin;Password=Blessme!1;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+        optionsBuilder.UseSqlServer("Server=tcp:sqlserver-siliconas.database.windows.net,1433;Initial Catalog=BlazorWebApp;Persist Security Info=False;User ID=SqlAdmin;Password=changeme$1234;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
 
         return new DataContext(optionsBuilder.Options);
     }
